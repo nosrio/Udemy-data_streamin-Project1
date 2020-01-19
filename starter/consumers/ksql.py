eb7ab643282d01b97dabffc1ee9753a6fc985207
Nico Osorio <nosorio@tenaris.com>
2020-01-19 19:16:26 -0300

    updating ksql.py

--- starter/consumers/ksql.py
+++ starter/consumers/ksql.py
@@ -23,14 +23,20 @@ KSQL_URL = "http://localhost:8088"
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
+    station_id BIGINT,
+    station_name VARCHAR,
+    line VARCHAR
 ) WITH (
-    ???
+    KAFKA_TOPIC = 'com.udacity.project1.turnstile',
+    VALUE_FORMAT = 'AVRO',
+    KEY = 'station_id'
 );
 
 CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+WITH (KAFKA_TOPIC = 'TURNSTILE_SUMMARY', VALUE_FORMAT = 'JSON') AS
+            SELECT station_id, COUNT(*) AS count
+            FROM turnstile
+            GROUP BY station_id;
 """
 
 
@@ -43,7 +49,8 @@ def execute_statement():
 
     resp = requests.post(
         f"{KSQL_URL}/ksql",
-        headers={"Content-Type": "application/vnd.ksql.v1+json"},
+        headers={"Content-Type": "application/vnd.ksql.v1+json",
+                "Accept": "application/vnd.ksql.v1+json"},
         data=json.dumps(
             {
                 "ksql": KSQL_STATEMENT,
